5613c60dcc98025f8af154364aa8fb4a9d41b72b
Henrik Engdahl <hellohanky@gmail.com>
2025-05-16 19:40:01 +0200

    Fix: Remove font exports from layout.tsx to fix Vercel deployment error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,5 @@
 import '@/app/ui/global.css';
-import { Inter, Lusitana } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
- 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
